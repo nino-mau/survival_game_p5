6f2764c38e974dd11c4e07a610e9af0f520865bb
Nino Mauuary <nino.mauuary@gmail.com>
2025-01-07 14:05:59 +0100

    Put css related files and sketch.js in public folder

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 
 const app = express();
 
+/** Serve static files to server */
+app.use(express.static('public'));
 
+/** Set responses */
 app.use((req, res, next) => {
     console.log('Requête reçue !');
     next();
@@ -23,5 +26,6 @@ app.use((req, res, next) => {
     console.log('Réponse envoyée avec succès !');
 });
   
+/** Export app object to be used in server.js */
 module.exports = app;
 
