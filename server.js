ffb985d99e19a8201658b715db5f1c4d3dfa8670
Nino Mauuary <nino.mauuary@gmail.com>
2025-01-07 10:57:40 +0100

    Install noedemon and express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
+    res.end('Voilà la réponse du serveur 2 !');
 });
 
 server.listen(process.env.PORT || 3000);
